1aea1e543a891aede2005c25b7f97ab08e262933
Harsh Mehta <109781812+harshmehta15@users.noreply.github.com>
2025-10-06 17:52:06 +0530

    Time: 7 ms (19.41%) | Memory: 18.2 MB (95.64%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,19 +4,29 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
 
-        rowref = [1 for i in range(rows)]
-        colref = [1 for j in range(cols)]
+        
+        col0 = 1
 
         for i in range(rows):
             for j in range(cols):
                 if(matrix[i][j]==0):
-                    rowref[i]=0
-                    colref[j]=0
+                    matrix[i][0] = 0
+                    if j == 0:
+                        col0 = 0
+                    else:
+                        matrix[0][j] = 0
         
-        for i in range(rows):
-            for j in range(cols):
-                if(rowref[i]==0 or colref[j]==0):
+        for i in range(1,rows):
+            for j in range(1,cols):
+                if(matrix[i][0]==0 or matrix[0][j]==0):
                     matrix[i][j]=0
+        
+        if(matrix[0][0]==0):
+            for i in range(cols):
+                matrix[0][i] = 0
+        if col0 ==0 :
+            for i in range(rows):
+                matrix[i][0] =0
 
 
         
