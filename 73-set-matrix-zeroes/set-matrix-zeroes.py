008b7a92908c715706cce1b35a95c881eb8006cf
Harsh Mehta <109781812+harshmehta15@users.noreply.github.com>
2025-10-06 17:21:37 +0530

    Time: 3 ms (71.03%) | Memory: 18.6 MB (38.07%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,27 +4,20 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
 
+        rowref = [1 for i in range(rows)]
+        colref = [1 for j in range(cols)]
+
         for i in range(rows):
             for j in range(cols):
-
                 if(matrix[i][j]==0):
-                    self.makerow(i,matrix)
-                    self.makecol(j,matrix)
+                    rowref[i]=0
+                    colref[j]=0
         
         for i in range(rows):
             for j in range(cols):
-
-                if(matrix[i][j]=='a'):
+                if(rowref[i]==0 or colref[j]==0):
                     matrix[i][j]=0
-        
-    def makerow(self,n,matrix):
-        for i in range(len(matrix[n])):
-            if(matrix[n][i]!=0):
-                matrix[n][i]='a'
 
-    def makecol(self,n,matrix):
-        for i in range(len(matrix)):
-            if(matrix[i][n]!=0):
-                matrix[i][n]='a'  
+
         
         
